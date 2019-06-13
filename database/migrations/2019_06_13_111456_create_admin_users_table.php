51ae59330a1c3b441a1876b4c05dabdf0d50d58b
abhishekgiri49 <abhishekgiri49@hotmail.com>
2019-06-13 17:10:34 +0545

    'migration_incomplete'

---
+++ database/migrations/2019_06_13_111456_create_admin_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin_users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('username')->unique();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->timestamps();
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')
+                    ->references('id')->on('admin_roles')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin_users');
+    }
+}
---
+++ database/migrations/2019_06_13_111542_create_module_permissions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateModulePermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('module_permissions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('code')->unique();
+            // $table->unsignedBigInteger('parent_id');
+            $table->foreign('parent_id')
+                    ->references('id')->on('module_permissions')
+                    ->onDelete('cascade');
+            $table->unsignedBigInteger('parent_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('module_permissions');
+    }
+}
