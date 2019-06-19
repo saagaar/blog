be94a006a8885dabb76bac43b9a0d7ebcbdddf03
abhishekgiri49 <abhishekgiri49@hotmail.com>
2019-06-19 17:19:32 +0545

    'dash'

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,30 @@
-@extends('layouts.app')
+@extends('layouts.common.main')
 
 @section('content')
-<div class="container">
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Page Header
+        <small>Optional description</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+        <li class="active">Here</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content container-fluid">
+
+      <!--------------------------
+        | Your Page Content Here |
+        -------------------------->
+
+    </section>
+    <!-- /.content -->
+  </div>
+<!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -19,5 +42,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 @endsection
