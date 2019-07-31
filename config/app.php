2eac0f59866eae44000ef0a9bd2c39dca51daf55
sagar chapagain <saagarchapagain@gmail.com>
2019-07-31 16:46:05 +0545

    'permission'

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\RepositoryServiceProvider::class,
         OwenIt\Auditing\AuditingServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
         
 
     ],
