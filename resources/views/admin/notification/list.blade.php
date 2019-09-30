6bf7f23582469223f5b43e85aa5b1f7c906231a6
abhishekgiri49 <abhishekgiri49@hotmail.com>
2019-09-30 12:06:33 +0545

    'email'

--- resources/views/admin/notification/list.blade.php
+++ resources/views/admin/notification/list.blade.php
@@ -6,16 +6,18 @@
           <div class="box">
             <div class="box-header">
               <h3 class="box-title">All notifications</h3>
-              <div class="box-tools">
-                <div class="input-group input-group-sm" style="width: 150px;">
-                  <a href="{{route('notification.create')}}" class="btn btn-primary">Add Notification Templete</a>
-                </div>
-              </div>
+              
             </div>
             <!-- /.box-header -->
             <div class="box-body">
              @component('layouts.components.search' )
              @endcomponent  
+               <div class="box-tools">
+                <div class="input-group input-group-sm" style="width: 150px;">
+                  <a href="{{route('notification.create')}}" class="btn btn-primary">Add Notification Templete</a>
+                </div>
+              </div>
+             
              <table id="example2" class="table table-bordered table-hover">
                 <thead>
                 <tr>
@@ -43,7 +45,7 @@
                   </td>
                   <td>{{ $eachNotification->subject }}</td>
                   <td>{{ $eachNotification->view }}</td>
-                  <td><?php echo(implode(',', $eachNotification->notification_type)); ?></td>
+                  <td> <span class="label label-info label-large"><?php echo(implode(',', $eachNotification->notification_type)); ?></span></td>
                   <td>{{$eachNotification->created_at}}</td>
                   <td><a href="{{route('notification.edit', $eachNotification->id)}}"><i class="fa fa-pencil-square-o"  aria-hidden="true"></i></a></td>
                   <td><a href="{{route('notification.delete', $eachNotification->id)}}"><i class="fa fa-trash" aria-hidden="true"></i></a></td>
