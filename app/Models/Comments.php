c2502a93fc3ca10b8eb1f1b885534ef6d78db2c5
abhishekgiri49 <abhishekgiri49@hotmail.com>
2019-10-22 10:31:45 +0545

    'like_comment'

---
+++ app/Models/Comments.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+use OwenIt\Auditing\Auditable as Auditables;
+class Comments extends Pivot implements Auditable
+{
+     use Auditables;
+    protected $table='comments';
+
+    protected $fillable = ['user_id','blog_id','comment','status'];
+
+
+    protected $hidden  = ['user_id','blog_id'];
+}
---
+++ resources/assets/js/layouts/Home.vue
@@ -0,0 +1,39 @@
+
+<template>
+<div>
+<TheTopNav></TheTopNav>
+<TheMainNav></TheMainNav>
+ 		<router-view></router-view>
+        <div class="clearfix"></div>
+<TheFooter></TheFooter>
+</div>
+</template>
+
+<script>
+  import { BulletListLoader,FacebookLoader } from 'vue-content-loader';
+import mixin  from './../mixins/LoadData.mixin.js';
+import TheTopNav from './../components/TopNav/TheTopNav';
+import TheMainNav from './../components/MainNav/TheMainNav';
+import TheFooter from './../components/Footer/TheFooter';
+    export default {
+        data() {
+          return {
+           initialState:{}
+           }
+        },
+        // name:UserDashboard,
+        mixin,
+        computed:{
+            me:function(){
+              return this.$store.getters.me
+            },
+            
+           
+        },
+        components:{
+            TheTopNav,
+            TheFooter,
+            TheMainNav
+        }
+    }
+</script>
