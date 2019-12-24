9ad6a33e2c4b1da72250b9f83b618d1e71a562a1
abhishek giri <abhishekgiri49.ag@gmail.com>
2019-12-24 10:42:10 +0545

    remember me

--- public/frontend/js/app.js
+++ public/frontend/js/app.js
@@ -8025,6 +8025,9 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
 //
 //
 //
+//
+//
+//
 
 
 
@@ -8037,7 +8040,8 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
     return {
       loginForm: new _services_Form_js__WEBPACK_IMPORTED_MODULE_5__["default"]({
         email: '',
-        password: ''
+        password: '',
+        remember: ''
       }),
       signUpForm: new _services_Form_js__WEBPACK_IMPORTED_MODULE_5__["default"]({
         email: '',
@@ -47560,6 +47564,61 @@ var render = function() {
                               : _vm._e()
                           ]
                         ),
+                        _vm._v(" "),
+                        _c("div", { staticClass: "form-group" }, [
+                          _c("input", {
+                            directives: [
+                              {
+                                name: "model",
+                                rawName: "v-model.trim",
+                                value: _vm.loginForm.remember,
+                                expression: "loginForm.remember",
+                                modifiers: { trim: true }
+                              }
+                            ],
+                            attrs: {
+                              type: "checkbox",
+                              name: "remember",
+                              id: "password1"
+                            },
+                            domProps: {
+                              checked: Array.isArray(_vm.loginForm.remember)
+                                ? _vm._i(_vm.loginForm.remember, null) > -1
+                                : _vm.loginForm.remember
+                            },
+                            on: {
+                              change: function($event) {
+                                var $$a = _vm.loginForm.remember,
+                                  $$el = $event.target,
+                                  $$c = $$el.checked ? true : false
+                                if (Array.isArray($$a)) {
+                                  var $$v = null,
+                                    $$i = _vm._i($$a, $$v)
+                                  if ($$el.checked) {
+                                    $$i < 0 &&
+                                      _vm.$set(
+                                        _vm.loginForm,
+                                        "remember",
+                                        $$a.concat([$$v])
+                                      )
+                                  } else {
+                                    $$i > -1 &&
+                                      _vm.$set(
+                                        _vm.loginForm,
+                                        "remember",
+                                        $$a
+                                          .slice(0, $$i)
+                                          .concat($$a.slice($$i + 1))
+                                      )
+                                  }
+                                } else {
+                                  _vm.$set(_vm.loginForm, "remember", $$c)
+                                }
+                              }
+                            }
+                          }),
+                          _vm._v("  Remember Me\n            ")
+                        ]),
                         _vm._v(" "),
                         _c(
                           "button",
