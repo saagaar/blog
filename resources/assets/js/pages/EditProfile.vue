ed4693421aadc130f52bb443a14e2e60b47043e1
sagar chapagain <saagarchapagain@gmail.com>
2019-12-24 14:37:20 +0545

    change profile image

--- resources/assets/js/pages/EditProfile.vue
+++ resources/assets/js/pages/EditProfile.vue
@@ -46,50 +46,14 @@
           <div class="col-md-6">
              <div class="col-sm-12 form-group upload_img">
               <label><i class="fa fa-image"></i> Profile Photo </label>
-           <!--    <cropper
-                        classname="cropper"
-                        :src="me.image? '/uploads/user-images/'+me.image:'/images/system-images/default-profile.png'"
-                        :stencilProps="{
-                          aspectRatio: 10/12
-                        }"
-                        :minWidth="48"
-                        :minHeight="72"
-                        :maxWidth="640"
-                        :maxHeight="760"
-                        @change="change"
-                        :previewClassname="image"
-                      >
-                      
-                </cropper> -->
-                <myUpload 
-                    @crop-success="cropSuccess"
-                    @crop-upload-success="cropUploadSuccess"
-                    @crop-upload-fail="cropUploadFail"
-                    v-model="show"
-                     lang-type="en"
-                :width="300"
-                :height="300"
-                field="image"
-                :header="headers"
-                :params="params"
-                url="/user/changeprofile"
-                ></myUpload>
-              <img :src="me.image? '/uploads/user-images/'+me.image:'/images/system-images/default-profile.png'" @click="toggleShow()">
-              <figure>
-                       <!-- <img :src="me.image? '/uploads/user-images/'+me.image:'/images/system-images/default-profile.png'" id="image-field"/>  -->
-                <span class="file-input btn btn-success btn-file">
-                      <button class="btn btn-success">Change Profile Picture</button> 
-                      <!-- <cropper
-                        classname="cropper"
-                        :src="me.image? '/uploads/user-images/'+me.image:'/images/system-images/default-profile.png'"
-                        :stencilProps="{
-                          aspectRatio: 10/12
-                        }"
-                        @change="change"
-                      ></cropper> -->
-                      <!-- <input type="file" ref="file" name="image" id="file1" class="upload" @change="toggleShow();"> -->
-                      <!-- <a ="#" @change="toggleShow()">Change Profile</a -->
-                  </span>
+              
+               <ProfileImage :profileChangeEnable="enableProfileChangePopup" @clicked="updatePopupClosed" >
+               </ProfileImage>
+
+              <figure @click="clickImageChange">
+                      <img :src="getProfileUrl()" alt="Profile Image" class="img-responsive" id="profileimage" />
+                
+                     
               </figure>   
           </div>
 
@@ -111,28 +75,17 @@ import TextEdit from './../components/Settings/TextEdit';
 import ChangePassword from './../components/Settings/ChangePassword';
 import EmailEdit from './../components/Settings/EmailEdit';
 import DobEdit from './../components/Settings/DobEdit';
+import ProfileImage from './../components/Profile/ProfileImage';
 import Form from './../services/Form.js';
-// import { Cropper } from 'vue-advanced-cropper'
-import myUpload from 'vue-image-crop-upload';
     export default {
          data() {
           return {
             isLoading:false,
+            enableProfileChangePopup:false,
             form:new Form({
               image:'',
               file:true
             }),
-            headers: {
-
-                        'Content-Type': 'multipart/form-data',
-                        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-                },
-            show:false,
-            params: {
-            name: 'image',
-          },
-          
-           
         }
       },
        
@@ -142,62 +95,71 @@ import myUpload from 'vue-image-crop-upload';
             },
         },
         methods:{
-        
-          toggleShow() 
-          {
-            this.show = !this.show;
+          clickImageChange(){
+            this.enableProfileChangePopup=true
           },
-          cropSuccess(image, field){
-            this.form.image = image;
+          updatePopupClosed(newval){
+            this.enableProfileChangePopup=newval
           },
-          /**
-           * upload success
-           *
-           * [param] jsonData  server api return data, already json encode
-           * [param] field
-           */
-          cropUploadSuccess(jsonData, field){
+          getProfileUrl(){
+                let url=this.me.image;
+                return this.$helpers.getProfileUrl(url);
+          },
+          // toggleShow() 
+          // {
+          //   this.show = !this.show;
+          // },
+          // cropSuccess(image, field){
+          //   this.form.image = image;
+          // },
+          // /**
+          //  * upload success
+          //  *
+          //  * [param] jsonData  server api return data, already json encode
+          //  * [param] field
+          //  */
+          // cropUploadSuccess(jsonData, field){
 
-             this.$store.commit('SETFLASHMESSAGE',{status:true,message:jsonData.message});
-             this.$store.commit('TOGGLE_LOADING');
-             this.$store.commit('UPDATE_PROFILE',jsonData.data.imageName);
-          },
-          /**
-           * upload fail
-           *
-           * [param] status    server api return error status, like 500
-           * [param] field
-           */
-          cropUploadFail(status, field){
-            console.log('-------- upload fail --------');
-            this.$store.commit('SETFLASHMESSAGE',{status:false,message:'We couldn\'t upload the image now.<br/> Please try again later.'});
-          },
+          //    this.$store.commit('SETFLASHMESSAGE',{status:true,message:jsonData.message});
+          //    this.$store.commit('TOGGLE_LOADING');
+          //    this.$store.commit('UPDATE_PROFILE',jsonData.data.imageName);
+          //    this.toggleShow()
+          // },
+          // /**
+          //  * upload fail
+          //  *
+          //  * [param] status    server api return error status, like 500
+          //  * [param] field
+          //  */
+          // cropUploadFail(status, field){
+          //   this.$store.commit('SETFLASHMESSAGE',{status:false,message:'We couldn\'t upload the image now.<br/> Please try again later.'});
+          // },
           changeImage:function() 
           {
-            let curObject=this;
-              const file = this.$refs.file.files[0];
-              if (file.size > 5 * 1024 * 1024) {
-                curObject.$store.commit('SETFLASHMESSAGE',{status:false,message:'File size is more then 5 MB'});
-                return;
-              }
-              this.form.image = file;
-            this.form.post('/user/changeprofile').then(response => {
-               if(response.data.status){
-                 curObject.$store.commit('SETFLASHMESSAGE',{status:true,message:response.data.message});
-                 // curObject.$store.commit('TOGGLE_LOADING');
-                 curObject.$store.commit('UPDATE_PROFILE',response.data.data.imageName);
-               }
-               else{
-                 curObject.$store.commit('SETFLASHMESSAGE',{status:false,message:response.data.message});
-                  // curObject.$store.commit('TOGGLE_LOADING');
-               }
-              }).catch(e => {
-                   curObject.$store.commit('TOGGLE_LOADING');
-                  if(e.status===false)
-                     curObject.$store.commit('SETFLASHMESSAGE',{status:false,message:e.message});
-                    else
-                   curObject.$store.commit('SETFLASHMESSAGE',{status:false,message:e.message});
-              });
+            // let curObject=this;
+            //   const file = this.$refs.file.files[0];
+            //   if (file.size > 5 * 1024 * 1024) {
+            //     curObject.$store.commit('SETFLASHMESSAGE',{status:false,message:'File size is more then 5 MB'});
+            //     return;
+            //   }
+            //   this.form.image = file;
+            // this.form.post('/user/changeprofile').then(response => {
+            //    if(response.data.status){
+            //      curObject.$store.commit('SETFLASHMESSAGE',{status:true,message:response.data.message});
+            //      // curObject.$store.commit('TOGGLE_LOADING');
+            //      curObject.$store.commit('UPDATE_PROFILE',response.data.data.imageName);
+            //    }
+            //    else{
+            //      curObject.$store.commit('SETFLASHMESSAGE',{status:false,message:response.data.message});
+            //       // curObject.$store.commit('TOGGLE_LOADING');
+            //    }
+            //   }).catch(e => {
+            //        curObject.$store.commit('TOGGLE_LOADING');
+            //       if(e.status===false)
+            //          curObject.$store.commit('SETFLASHMESSAGE',{status:false,message:e.message});
+            //         else
+            //        curObject.$store.commit('SETFLASHMESSAGE',{status:false,message:e.message});
+            //   });
 
           }
         },
@@ -207,7 +169,7 @@ import myUpload from 'vue-image-crop-upload';
             EmailEdit,
             ChangePassword,
             DobEdit,
-            myUpload
+            ProfileImage
         },
     }
 </script>
